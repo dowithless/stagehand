f32cfef182ce9a173b04a6062a086584e14f487c
Sean McGuire <seanmcguire1@outlook.com>
2025-06-09 23:36:15 -0700

    fix root

--- scripts/generate-version-file.mjs
+++ scripts/generate-version-file.mjs
@@ -5,7 +5,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const root = path.resolve(__dirname, "../..");
+const root = path.resolve(__dirname, "..");
 
 await fs.mkdir(path.join(root, "lib"), { recursive: true });
 
